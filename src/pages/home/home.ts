38e416c2a0dc13228513547f0409b15e27f09515
danicoronado <nuevacuentapami123@gmail.com>
2026-02-10 23:14:43 -0600

    fix: hide Play button when no trailer is available instead of disabling it

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-interface Contacto{
+interface Contacto {
   nombre: string;
   telefono: string;
   email: string;
-  redesSociales : string[];
+  redesSociales: string[];
   visualizarNumero: boolean;
 }
 
@@ -19,27 +19,27 @@ interface Contacto{
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home { 
+export class Home {
   showContent = false;
   rol = 'user';
-  diasSemana = ['Lunes', 'Martes','Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+  diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
   contactos: Contacto[] = [
-    {nombre: 'Jose Luis', telefono: '9513462874', email: 'cheluisojeda@gmail.com', redesSociales:['Facebook', 'Instagram'], visualizarNumero: false},
-    {nombre: 'Angel Obed', telefono: '9514180455', email: 'angelobed@gmail.com', redesSociales:['Instagram'], visualizarNumero: false},
-    {nombre: 'Tania Judith', telefono: '9511979804', email: 'taniajudith@gmail.com', redesSociales:['Facebook', 'Instagram', 'X'], visualizarNumero: false}
+    { nombre: 'Jose Luis', telefono: '0000000000', email: 'correo1@gmail.com', redesSociales: ['Facebook', 'Instagram'], visualizarNumero: false },
+    { nombre: 'Angel Obed', telefono: '0000000000', email: 'correo2@gmail.com', redesSociales: ['Instagram'], visualizarNumero: false },
+    { nombre: 'Josue Daniel', telefono: '0000000000', email: 'correo3@gmail.com', redesSociales: ['Facebook', 'Instagram', 'X'], visualizarNumero: false }
   ];
-  
 
-  verSpoiler(){
+
+  verSpoiler() {
     this.showContent = !this.showContent
   }
-  
-  serAdmin(){
+
+  serAdmin() {
     this.rol = 'admin'
   }
 
   verNumero(contacto: Contacto) {
-  contacto.visualizarNumero = !contacto.visualizarNumero;
+    contacto.visualizarNumero = !contacto.visualizarNumero;
   }
 
   estadoEnvio: string = 'pendiente';
